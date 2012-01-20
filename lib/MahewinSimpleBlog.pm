f7afa6e70efb78f3b3b2784f1dd1d967993d49f5
Hobbestigrou <hobbestigrou@erakis.im>
2012-01-21 00:46:45 +0100

    [Core] MahewinSimpleBlog: Refresh to see last comments and articles.

--- lib/MahewinSimpleBlog.pm
+++ lib/MahewinSimpleBlog.pm
@@ -78,6 +78,13 @@ get '/feed/:tag' => sub {
     _create_feed($get_articles);
 };
 
+get '/refresh' => sub {
+    $articles->clear_articles;
+    $comments->clear_comments;
+
+    redirect '/';
+};
+
 sub _create_feed {
     my ( $articles_list ) = @_;
 
